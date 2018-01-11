f549f2b0416bf91502901618c3eee7a528c0abec
Karthik <karthiknayakb@gmail.com>
2018-01-11 13:14:24 +0530

    initial commit

---
+++ javascript/agecalc.js
@@ -0,0 +1,3 @@
+var age = prompt("enter your age");
+
+alert(age+" is approximately "+(age*365)+" days");
---
+++ javascript/firstjs.js
@@ -0,0 +1,6 @@
+var fn = prompt("What is your first name?");
+var ln = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+console.log("Your name is "+fn+" "+ln+".");
+console.log("You are "+age+" years old.")
