701143b03587b6b7f0e4ef9c4f773fb9d9aad075
Karthik <karthiknayakb@gmail.com>
2018-02-01 12:54:07 +0530

    modified read file

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-fetch('index.txt')
-  .then(response => response.text())
-  .then(text => console.log(text))
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+console.log(readTextFile("links.txt"));
